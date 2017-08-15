f5608bcb7f26222d85351907dc7bc58814a461de
Anders <anders.raldin@gmail.com>
2017-08-15 22:41:51 +0200

    Create disallowed.php

---
+++ disallowed.php
@@ -0,0 +1,1 @@
+<?php $forbidden = array("exec", "system", "passthru", "shell_exec", "proc_open", "popen", "backticks", "disallowed.php", "assert", "eval", "pcntl_exec", "pcntl_exec", "include", "include_once", "require", "require_once"); ?>
